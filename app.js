a676a36d273d90f2b27ec73860af0a6c5b956379
prince04kumar <princesocial04@gmail.com>
2024-12-05 09:28:26 +0530

    changes

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ const accesstoken = keyValuePairs[0].value;
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const challenge = req.query["hub.challenge"];
-   const mytoken = req.query["hub.verify_token"];
+  const mytoken = req.query["hub.verify_token"];
 
   if (mode && mytoken) {
     if (mode === "subscribe" && mytoken === mytoken) {
       console.log("Verified");
-      
       res.status(200).send(challenge);
     } else {
       res.status(403).send("Forbidden");
@@ -38,75 +37,91 @@ app.post("/webhook", async (req, res) => {
       body_param.entry[0].changes[0].value.messages[0]
     ) {
       const message = body_param.entry[0].changes[0].value.messages[0];
-
-      //const msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
       const phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
       const messageId = message.id;
       const from = message.from;
-     // console.log("Message: " + msg_body);
-      console.log("Phone Number ID: " + phone_no_id);
+      console.log("From: " + from);
       console.log("Message ID: " + messageId);
-     
-
 
       let response_message = "Hello! How can I help you?";
-
-
-          
-      if (processedMessages.has(messageId)){
-        return res.sendStatus(200); 
+      if (processedMessages.has(messageId)) {
+        return res.sendStatus(200);
       }
       processedMessages.add(messageId);
+      let msg_type;
+      
+      // Identify the message type and msg
+      if (message.text) {
+        msg_type = "text";
+        const msg_body_text = message.text.body;
+        console.log("Msg type:", msg_type, "\nMsg body:", msg_body_text);
+      }
+      
+      
+      else if (message.image) {
+        msg_type = "image";
+        const msg_body_imageId = message.image.id;
+        console.log("Msg type:", msg_type, "Msg body:", msg_body_imageId);
 
-   // Identify the message type
 
-   if (message.text) {
-    console.log(" Message Type : text " + message.text.body);
-    response_message = "You sent a text message.";
-  } else if (message.image) {
-    console.log("Message Type : image " + message.image.id);
-    response_message = "You sent an image.";
-  } else if (message.document) {
-    console.log("Message Type : docs " + message.document.id);
-    response_message = "You sent a document.";
-  } else if (message.video) {
-    console.log("Message Type : Video msg " + message.video.id);
-    response_message = "You sent a video.";
-  }  else if (message.audio) {
-    console.log("Message Type : audio msg " + message.audio.id);
-    response_message = "You sent an audio.";
-  } else {
-    console.log("Unknown Message Type");
-    response_message = "I'm not sure what you sent.";
-  }
+         // Fetch the image and convert it to Base64
+         try {
+          const imageResponse = await axios({
+            method: "GET",
+            url: `https://graph.facebook.com/v13.0/${msg_body_imageId}/?access_token=${accesstoken}`,
+            responseType: "arraybuffer",
+            headers: {
+              Authorization: `Bearer ${accesstoken}`,
+            },
+          });
 
-     // to send msg
-     axios({
-      method: "POST",
-      url: `https://graph.facebook.com/v13.0/${phone_no_id}/messages?access_token=${accesstoken}`,
-      data: {
-        messaging_product: "whatsapp",
-        to: from,
-        text: {
-          body: response_message,
+          // Convert the image to Base64
+          const base64EncodedImage = Buffer.from(imageResponse.data, "binary").toString("base64");
+          const filePart = {
+            inline_data: {
+              data: base64EncodedImage,
+              mimeType: 'image/jpeg',
+            },
+          };
+          console.log("File part created:",  typeof filePart);
+
+        } catch (error) {
+          console.error("Error fetching or converting image:", error.message);
+        }
+      }
+      
+
+      // to send msg
+      axios({
+        method: "POST",
+        url: `https://graph.facebook.com/v13.0/${phone_no_id}/messages?access_token=${accesstoken}`,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: {
+            body: response_message,
+          },
         },
-      },
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        console.log("Message sent:");
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((error) => {
-        console.error("Error sending message:", error.response ? error.response.data : error.message);
-      });
-  }
-
+        .then((response) => {
+          console.log("Message sent:");
+        })
+        .catch((error) => {
+          console.error(
+            "Error sending message:",
+            error.response ? error.response.data : error.message
+          );
+        });
+    }
   } else {
     res.sendStatus(400);
   }
 });
+
+
 
 // Start the server
 app.listen(port, () => {
